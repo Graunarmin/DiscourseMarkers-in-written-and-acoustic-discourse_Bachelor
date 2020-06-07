dcf51381083b589eb78d0da35bd377f950f3a20e
Johanna Sacher <johanna.sacher@uni-weimar.de>
2020-06-07 17:29:18 +0200

    comments

--- Scripte/CorpusParser.py
+++ Scripte/CorpusParser.py
@@ -11,12 +11,18 @@ class CorpusParser:
         self.irregularObjects = []
 
     def read_lines(self):
+        '''read in the file line by line, processing each line before loading the next one'''
+
         json_object = None
+
         with open(self.filename) as file:
             for cnt, row in enumerate(file):
                 line = row.strip()
                 #print("Line {}: {}".format(cnt, line))
                 flag = self.get_flag(line)
+
+                '''determine whether the line is a complete json object
+                or just the beginning/middle/end of one'''
                 if flag is "c" and json_object is None: 
                     json_object = line
                     self.process_json(json_object)
@@ -33,12 +39,14 @@ class CorpusParser:
         self.process_output()
 
     def process_json(self, json_object):
+        '''load the string as json and extract the needed information'''
         json_obj = json.loads(json_object)
 
         #self.extract_cities(json_obj)
         self.extract_shownames(json_obj)
 
     def extract_cities(self, json_obj):
+        '''extract city-information and write them to a file'''
         try:
             if json_obj["city"] in self.cities:
                 self.cities[json_obj["city"]]["Counter"] += 1
@@ -52,6 +60,7 @@ class CorpusParser:
             self.irregulObjects.append(json_obj)
 
     def extract_shownames(self, json_obj):
+        '''extract show_name-information and write them to a file'''
         try:
             if json_obj["show_name"] in self.show_names:
                 self.show_names[json_obj["show_name"]] += 1
@@ -60,7 +69,6 @@ class CorpusParser:
         except KeyError as key:
             json_obj["ShowKeyError"] = format(key)
             self.irregularObjects.append(json_obj)
-        self.write_json("shows.json", )
 
     def process_output(self):
         #write_json("cities.json", self.show_names)
@@ -68,8 +76,11 @@ class CorpusParser:
         self.write_json("irregular_objects.json", self.irregularObjects)
 
     def write_json(self, out_filename, data):
+        '''write json-object to file'''
         with open(out_filename, 'w') as outfile:
             json.dump(data, outfile,indent=2)
+
+    '''Helper functions to determine wheter the string is a completed json-object'''
 
     def get_flag(self, line):
         if self.is_complete_object(line):
@@ -82,7 +93,6 @@ class CorpusParser:
             return "m"
         else:
             return "f"
-
 
     def is_complete_object(self, line):
         return self.is_start_of_object(line) and self.is_end_of_object(line)
