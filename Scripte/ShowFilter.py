e6e87ab726ed919912acd056492d14ea447c7a95
Johanna Sacher <johanna.sacher@uni-weimar.de>
2020-06-10 23:02:50 +0200

    extracted callsigns

--- Scripte/ShowFilter.py
+++ Scripte/ShowFilter.py
@@ -1,11 +1,14 @@
 import json
+import sys
 
 class ShowFilter:
 
-    def __init__(self):
-        self.in_file = "../bigData/shows.json"
-        self.out_file = "../data/filtered_shows.json"
+    def __init__(self, data, outfile):
+        self.in_file = data
+        self.out_file = outfile
         self.filtered_shows = {}
+        
+        self.read_in_shows()
 
     def read_in_shows(self):
         '''read in all shows as json object'''
@@ -16,7 +19,6 @@ class ShowFilter:
         self.filter_shows(shows)
         self.write_output()
 
-        
 
     def filter_shows(self, shows):
         '''go through json and put them all into a dict:
@@ -24,11 +26,20 @@ class ShowFilter:
 
         for key in shows:
             if key in self.filtered_shows:
-                self.filtered_shows[key] += shows[key]
+                self.filtered_shows[key][total] += shows[key][1]
             else:
-                self.filtered_shows[key] = shows[key]
+                self.filtered_shows[key] = {}
+                self.filtered_shows[key]["total"] = shows[key][1]
+                self.filtered_shows[key]["callsign"] = shows[key][0]
     
     def write_output(self):
         '''write output to json or pickle or csv'''
         with open(self.out_file, 'w') as outfile:
             json.dump(self.filtered_shows, outfile, indent=2)
+
+
+def main():
+    show_filter = ShowFilter(sys.argv[1], sys.argv[2])
+
+if __name__ == '__main__':
+    main()
