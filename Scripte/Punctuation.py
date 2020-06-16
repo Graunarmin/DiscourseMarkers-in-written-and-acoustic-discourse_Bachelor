79451c73e18f9e637f6853bc0f8d091578c35a00
Johanna Sacher <johanna.sacher@uni-weimar.de>
2020-06-16 18:46:20 +0200

    add post-processing punctuation

--- Scripte/Punctuation.py
+++ Scripte/Punctuation.py
@@ -7,27 +7,44 @@ class Punctuation():
     def __init__(self, data, outfile):
         self.data_file = data
         self.outfile = outfile
+        self.segmenter = DeepSegment('en')
         
 
     def load_json_objects(self):
         with open (self.data_file) as file:
             for row in file:
                 json_object = json.loads(row)
+                #there is only one key (show) in everey json,
+                #but we still need to "loop" to get it
                 for show in json_object:
-                    pass
 
-                self.add_punctuation(json_object)
+                    json_object[show] = self.add_punctuation(json_object[show])
+                        
+                    self.write_show_content(json_object)
 
-    def add_punctuation(self, object):
-        pass
+    def add_punctuation(self, json_obj):
 
-    def write_text_to_file(self, text):
-        pass
+        texts = {}
+        for date in json_obj:
+            sentences = self.segmenter.segment(json_obj[date])
+            text = ""
+            for sentence in sentences:
+                text += sentence
+                text += ". "
+            texts[date] = text
+        
+        return texts
+
+    def write_show_content(self, show):
+        with open(self.outfile, 'a') as outfile:
+            json.dump(show)
+            outfile.write("\n")
+
 
 def main():
     '''
-    Argument 1: json file that holds the snippets without punctuation
-    Argument 2: out file name for the texts with punctuation
+    Argument 1: json file that holds the snippets without punctuation (../bigData/texts_bare.json)
+    Argument 2: out file name for the texts with punctuation (../bigData/texts_punctuation.json)
     '''
 
     punctuation = Punctuation(sys.argv[1], sys.argv[2])
