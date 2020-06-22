95528ab2e2e8de8bda780a136cc8f89971cf8e14
Johanna Sacher <johanna.sacher@uni-weimar.de>
2020-06-22 16:32:30 +0200

    error in arguments

--- Scripte/TestData.py
+++ Scripte/TestData.py
@@ -84,7 +84,7 @@ def main():
     Argument 3: outfile name
     '''
 
-    testData = TestData(sys.argv[1], sys.argv[2], sys.argv[2])
+    testData = TestData(sys.argv[1], sys.argv[2], sys.argv[3])
 
 if __name__ == '__main__':
     main()
